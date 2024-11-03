e6c31a3fd0fe121a4c97ca68388d88aa175f648f
Akapi895 <83953516+Akapi895@users.noreply.github.com>
2024-11-03 12:34:04 +0700

    add route: generate_weekly_menu

---
+++ backend/Weekly_menu/controller.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+from .services import generate_weekly_menu
+
+weekly_menu = Blueprint("weekly_menu", __name__)
+
+@weekly_menu.route("/api/weekly_menu", methods=['GET'])
+def weekly_menu_api(user_id):
+    generate_weekly_menu(user_id=user_id)
