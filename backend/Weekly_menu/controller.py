49682d772fed20584b925dadb2bd29fe967874ba
Akapi895 <83953516+Akapi895@users.noreply.github.com>
2024-11-10 14:58:27 +0700

    Change position

--- backend/Weekly_menu/controller.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from .services import generate_weekly_menu
-
-weekly_menu = Blueprint("weekly_menu", __name__)
-
-@weekly_menu.route("/api/weekly_menu", methods=['GET'])
-def weekly_menu_api(user_id):
-    return generate_weekly_menu(user_id=user_id)
