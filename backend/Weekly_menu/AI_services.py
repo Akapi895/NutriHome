3d4ffc3f1ec7b6510136940dfeb0765c538135df
Akapi895 <83953516+Akapi895@users.noreply.github.com>
2024-11-10 08:14:19 +0700

    Feat: Update weekly menu for each members

--- backend/Weekly_menu/AI_services.py
+++ backend/Weekly_menu/AI_services.py
@@ -10,21 +10,56 @@ from dotenv import load_dotenv
 def connect_db(db_name, timeout=30):
     return sqlite3.connect(db_name, timeout=timeout)
 
-def get_all_recipes(conn):
+# Lấy ra những món ăn đã được lưu trước trong tuần này (gồm 7 ngày)
+def get_current_meal(conn, user_id, day=datetime.datetime.now().date()):
     cursor = conn.cursor()
-    cursor.execute("SELECT recipe_id, name, protein, fat, carbs, calories FROM recipes")
+    cursor.execute("""
+        SELECT recipe_id
+        FROM eating_histories
+        WHERE user_id = ? AND day >= ? AND eaten = 0
+    """, (user_id, day))
     recipes = cursor.fetchall()
-    
-    def format_recipes(recipes):
-        formatted_recipes = []
-        for recipe in recipes:
-            formatted_recipes.append(f"ID: {recipe[0]}, Name: {recipe[1]}, Protein: {recipe[2]}g, Fat: {recipe[3]}g, Carbs: {recipe[4]}g, Calories: {recipe[5]}")
-        return "\n".join(formatted_recipes)
-    
-    return format_recipes(recipes)
+    current_meal = [recipe[0] for recipe in recipes]
 
-# Gen ra thực đơn bằng AI
-def get_meal_plan_from_gemini(user_info, available_meals):
+    meal_details = []
+    for recipe_id in current_meal:
+        cursor.execute("""
+            SELECT name, carbs, protein, fat, calories
+            FROM recipes
+            WHERE recipe_id = ?
+        """, (recipe_id,))
+        recipe = cursor.fetchone()
+        if recipe:
+            name, carbs, protein, fat, calories = recipe
+            meal_details.append(f"Tên món có id {recipe_id} là {name}, với lượng chất dinh dưỡng là {carbs}g carbs, {protein}g protein, {fat}g fat và {calories} calories")
+    
+    meal_details_str = "; ".join(meal_details)
+
+    return meal_details_str
+
+# Lấy thông tin cá nhân của user
+def get_user_calo(conn, user_id):
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT target_protein, target_fat, target_carbs, target_calories
+        FROM users
+        WHERE user_id = ?
+    """, (user_id,))
+    user_calo = cursor.fetchone()
+    if user_calo:
+        user_calo = {
+            'target_protein': user_calo[0],
+            'target_fat': user_calo[1],
+            'target_carbs': user_calo[2],
+            'target_calories': user_calo[3]
+        }
+        return user_calo
+    else:
+        print(f"Không tìm thấy thông tin cá nhân của user {user_id}")
+        return None
+
+# Gen ra thực đơn bằng AI 
+def personal_menu(user_id, user_calo, available_meals, current_meal):
     load_dotenv()
     API_KEY = os.getenv("GEMINI_API_KEY")
     API_URL = os.getenv("GEMINI_API_URL")
@@ -32,10 +67,10 @@ def get_meal_plan_from_gemini(user_info, available_meals):
         'Content-Type': 'application/json'
     }
     
-    daily_protein = user_info['daily_protein']
-    daily_fat = user_info['daily_fat']
-    daily_carbs = user_info['daily_carbs']
-    daily_calories = user_info['daily_calories']
+    target_protein = user_calo['target_protein']
+    target_fat = user_calo['target_fat']
+    target_carbs = user_calo['target_carbs']
+    target_calories = user_calo['target_calories']
     today = datetime.datetime.now().date() 
 
     data = {
@@ -44,22 +79,24 @@ def get_meal_plan_from_gemini(user_info, available_meals):
                 "parts": [
                     {
                         "text": (
-                            f"Bạn là một chuyên gia dinh dưỡng. " 
-                            f"Hãy tạo thực đơn cho cả tuần (7 ngày) cho một người tính từ ngày {today}, "
-                            f"mỗi ngày gồm 3 bữa (sáng, trưa, tối) đáp ứng nhu cầu dinh dưỡng hàng ngày sau: "
-                            f"{daily_protein}g protein, {daily_fat}g fat, {daily_carbs}g carbs và {daily_calories} calories. "
-                            f"Sử dụng chỉ các món ăn có sẵn sau đây (với thông tin dinh dưỡng được cung cấp dưới dạng JSON, "
-                            f"bao gồm lượng calo, protein, carbs, fat cho mỗi món): {available_meals}. "
-                            f"Yêu cầu bổ sung:"
-                            f"Mỗi bữa có 3-5 món (bữa sáng: 1-2 món). "
-                            f"Đa dạng thực đơn: Hạn chế tối đa sự lặp lại món ăn trong cùng một ngày và trong cả tuần. "
-                            f"Thực đơn cần cân đối đủ đạm, tinh bột, chất béo và chất xơ; không nên chỉ có một nhóm thực phẩm để đảm bảo dinh dưỡng và năng lượng. "
-                            f"Bữa sáng cần nhanh gọn, đủ chất dinh dưỡng với protein, chất xơ, và tinh bột để duy trì năng lượng, "
-                            f"bữa sáng thường có một trong các món sau: bánh mì, phở, bún, cháo, xôi, bánh cuốn, mì, cơm. "
-                            f"Bữa trưa cần đủ đạm, rau xanh, và tinh bột để cung cấp năng lượng cho buổi chiều, tránh thức ăn quá dầu mỡ dễ gây buồn ngủ. "
-                            f"Bữa tối nên nhẹ nhàng, ít tinh bột và dầu mỡ, tập trung vào rau xanh và đạm dễ tiêu để cơ thể thư giãn, dễ ngủ. "
-                            f"Trả về kết quả ở định dạng JSON, chỉ bao gồm recipe_id của các món ăn được chọn cho mỗi bữa ăn trong 7 ngày "
-                            f"và không chứa thông tin dinh dưỡng của từng món ăn."
+                            f"""Bạn là một chuyên gia dinh dưỡng chuyên xây dựng thực đơn.  
+                            Hãy bổ sung thực đơn tuần (7 ngày) cho tôi từ những món ăn đã có sẵn: {current_meal}. 
+                            Thực đơn sau khi bổ sung cần đáp ứng lượng chất cần thiết của cá nhân tôi lần lượt là 
+                            {target_protein}g protein, {target_fat}g fat, {target_carbs}g carbs và {target_calories} calories. 
+                            Sử dụng chỉ các món ăn có sẵn sau đây (với thông tin dinh dưỡng được cung cấp dưới dạng JSON, 
+                            bao gồm lượng calo, protein, carbs, fat cho mỗi món): {available_meals}. 
+                            Hạn chế tối đa sự lặp lại món ăn trong cùng một ngày và trong cả tuần, tính toán để phù hợp với sở thích ăn uống của độ tuổi của tôi.
+                            Thực đơn cần cân đối đủ đạm, tinh bột, chất béo và chất xơ; không nên chỉ có một nhóm thực phẩm. 
+                            Bữa sáng cần nhanh gọn, đủ chất dinh dưỡng với protein, chất xơ, và tinh bột, 
+                            thường có một trong các món sau: bánh mì, phở, bún, cháo, xôi, bánh cuốn, mì, cơm.  
+                            Bữa trưa cần đủ đạm, rau xanh, và tinh bột, tránh thức ăn quá dầu mỡ. 
+                            Bữa tối nên nhẹ nhàng, ít tinh bột và dầu mỡ, tập trung vào rau xanh và đạm dễ tiêu. 
+                            Trả về kết quả ở định dạng JSON, chứa thông tin chi tiết về các món ăn được bổ sung, 
+                            không giải thích hay có thông tin gì thêm. 
+                            Không cần thêm \n hoặc \t vào kết quả trả về. 
+                            Với eaten mặc định bằng 0, user_id là {user_id} lấy trong thông tin cá nhân của tôi. 
+                            Kết quả trả về dưới dạng các object, ví dụ gồm: "user_id": 1, "recipe_id": 34, "day": '2024-11-09', "meal": "lunch", "eaten": 0.
+                            """
                         )
                     }
                 ]
@@ -71,75 +108,50 @@ def get_meal_plan_from_gemini(user_info, available_meals):
     
     if response.status_code == 200:
         try:
-            response_data = response.json()  # Trả về dữ liệu dưới dạng dict nếu response là JSON
-            return response_data
+            clean_string = response.json()['candidates'][0]['content']['parts'][0]['text'][7:-3]
+            if type(clean_string) == str:
+                data = json.loads(clean_string)
+                return data
+            else:
+                print("String không hợp lệ")
+                return None
         except json.JSONDecodeError:
-            return response.text  # Trả về chuỗi nếu JSON không hợp lệ
+            return response.text  
     else:
         print(f"Lỗi khi gọi API Gemini: {response.status_code}")
         print(response.text)
         return None
 
-# Chuyển chuỗi JSON thành dictionary Python
-def format_meal_plan(meal_plan):
-    text_content = meal_plan['candidates'][0]['content']['parts'][0]['text']
-    
-    match = re.search(r'```json\n({.*?})\n```', text_content, re.DOTALL)
-    if match:
-        meal_plan_json = json.loads(match.group(1))
-        # print(json.dumps(meal_plan_json, indent=4))
-        return meal_plan_json
-    else:
-        print("Không tìm thấy nội dung JSON trong meal_plan.")
-        return None
-
-# Lấy thông tin người dùng 
-def get_user_info(user_id, conn):
+# Lấy thông tin các món ăn được gợi ý chung
+def get_available_meals(conn, user_id, day=datetime.datetime.now().date()):
     cursor = conn.cursor()
     cursor.execute("""
-        SELECT target_protein, target_carbs, target_fat, target_calories 
-        FROM users
-        WHERE user_id = ?
-    """, (user_id,))
-    
-    user_data = cursor.fetchone()
+        SELECT recipe_id FROM eating_histories
+        WHERE user_id = ? AND day >= ? AND eaten = 0
+    """, (user_id, day))
+    recipes = cursor.fetchall()
+    available_meals = [recipe[0] for recipe in recipes]
+    return available_meals
 
-    if not user_data:
-        return None
+def bonus_meal(user_id):
+    conn = connect_db("../nutrihome.db")
+    available_meals = get_available_meals(conn, user_id)
+    current_meal = get_current_meal(conn, user_id)
+    user_calo = get_user_calo(conn, user_id)
 
-    daily_protein = user_data[0]
-    daily_carbs = user_data[1]
-    daily_fat = user_data[2]
-    daily_calories = user_data[3]
+    bonus_meal_plan = personal_menu(user_id, user_calo, available_meals, current_meal)
 
-    user_info = {
-        "daily_protein": daily_protein,
-        "daily_carbs": daily_carbs,
-        "daily_fat": daily_fat,
-        "daily_calories": daily_calories
-    }
-    return user_info
-
-# Thêm thực đơn hàng tuần vào db
-def import_weekly_menu(data, user_id, conn):
     cursor = conn.cursor()
-    cursor.execute(
-        """
-        SELECT eating_history_id FROM eating_histories
-        ORDER BY eating_history_id DESC
-        LIMIT 1
-        """
-    )
-    last_id = cursor.fetchone()
-    last_id = last_id[0] if last_id else 0
+    insert_query = """
+    INSERT INTO eating_histories (user_id, recipe_id, day, meal, eaten) 
+    VALUES (:user_id, :recipe_id, :day, :meal, :eaten)
+    """
+    try:
+        cursor.executemany(insert_query, bonus_meal_plan)
+        conn.commit()
+    except sqlite3.Error as e:
+        print("Lỗi khi chèn dữ liệu:", e)
 
-    for day, meals in data.items():
-        for meal_type, recipe_ids in meals.items():
-            for recipe_id in recipe_ids:
-                last_id += 1
-                cursor.execute("""
-                    INSERT INTO eating_histories (eating_history_id, user_id, recipe_id, day, meal, eaten)
-                    VALUES (?, ?, ?, ?, ?, ?)
-                """, (last_id, user_id, recipe_id, day, meal_type.lower(), 0))
-                
-    conn.commit() 
+
+if __name__ == "__main__":
+    bonus_meal(1)
--- backend/Weekly_menu/services.py
+++ backend/Weekly_menu/services.py
@@ -1,37 +1,4 @@
-from AI_services import connect_db, get_user_info, get_all_recipes, get_meal_plan_from_gemini, format_meal_plan, import_weekly_menu
-import json
+from AI_services import bonus_meal
 
-# Hàm tạo thực đơn hàng tuần với user_id
-def generate_weekly_menu(user_id):
-    conn = connect_db("../nutrihome.db")
-    cursor = conn.cursor()
-    
-    user_info = get_user_info(user_id, conn)
-    available_meals = get_all_recipes(conn) # str
-        
-    meal_plans = get_meal_plan_from_gemini(user_info, available_meals) # dict
-
-    if meal_plans:
-        try:
-            formatted_plan = format_meal_plan(meal_plans)
-            if formatted_plan:
-                import_weekly_menu(formatted_plan, user_id, conn)
-            else:
-                print("Failed to format meal plan")
-        except json.JSONDecodeError as e:
-            print(f"Lỗi phân tích JSON: {e}")
-    else:
-        print("Failed to get meal plans")
-    conn.close()
-
-# Xóa các bản ghi trước một ngày nhất định
-def delete_records_before_date(conn, target_date):
-    cursor = conn.cursor()
-    
-    cursor.execute(""" 
-        DELETE FROM eating_histories
-        WHERE day < ?
-    """, (target_date,))
-    
-    conn.commit()
-
+def AI_generate_bonus_meal(user_id):
+    bonus_meal(user_id)
